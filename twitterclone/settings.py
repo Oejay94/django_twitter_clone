e5b8b32dd6d2be1dfcc8d23b195dca8a7273a703
Joseph Brown <joseph.d.brown94@gmail.com>
2020-03-06 13:51:04 -0500

    Begun styling the Twitter Clone

--- twitterclone/settings.py
+++ twitterclone/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'twitteruser.apps.TwitteruserConfig',
+    'bootstrap4',
     'tweet.apps.TweetConfig',
     'notification.apps.NotificationConfig',
     'authentication.apps.AuthenticationConfig',
