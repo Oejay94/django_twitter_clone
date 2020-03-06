514768f256a33206ba74f1bd79e29caeb92dce34
Joseph Brown <joseph.d.brown94@gmail.com>
2020-03-06 10:55:13 -0500

    switched out function views for Class based views for the PostTweet, TweetPage, LoginView, and CreateAccount

--- authentication/views.py
+++ authentication/views.py
@@ -1,14 +1,38 @@
 from django.shortcuts import render, reverse, HttpResponseRedirect
 from django.contrib.auth import login, logout, authenticate
+from django.views import View
 
 from .forms import login_form
 
 from twitteruser.models import CustomUser
 
-def login_view(request):
-    html = 'main.html'
+# def login_view(request):
+#     html = 'main.html'
 
-    if request.method == 'POST':
+#     if request.method == 'POST':
+#         form = login_form(request.POST)
+
+#         if form.is_valid():
+#             data = form.cleaned_data
+#             user = authenticate(
+#                 username=data['username'],
+#                 password=data['password']
+#             )
+#             if user is not None:
+#                 login(request, user)
+#                 return render(request, 'user_page.html')
+#     else:
+#         form = login_form()
+
+#     return render(request, html, {'login': form})
+
+class LoginView(View):
+    def get(self, request):
+        html = 'main.html'
+        form = login_form()
+        return render(request, html, {'login': form})
+
+    def post(self, request):
         form = login_form(request.POST)
 
         if form.is_valid():
@@ -20,12 +44,9 @@ def login_view(request):
             if user is not None:
                 login(request, user)
                 return render(request, 'user_page.html')
-    else:
-        form = login_form()
-
-    return render(request, html, {'login': form})
 
 
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect(request.GET.get('next', '/login/'))
+
